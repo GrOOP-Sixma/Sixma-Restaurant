7554d9026477c643cab534652f996a1ac265fd19
Jovian Lim <kiriketsuki@gmail.com>
2021-11-13 02:30:32 +0800

    oopsie

--- src/Table/TableFactory.java
+++ src/Table/TableFactory.java
@@ -97,7 +97,7 @@ public class TableFactory {
         // if there are no tables, create tables
         if (tableController.getTableMap().size() == 0) {
             for (int i = 2; i < 10; i ++) {
-                if (i % 2 == 0) {
+                if (i % 2 != 0) {
                     i++;
                 }
                 for (int j = 1; j < 10; j++) {
